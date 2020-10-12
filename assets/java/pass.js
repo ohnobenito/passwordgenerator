e2268fc34fa1de42777a3b6e901163e6a0ff3f70
Benito Gonzalez <ohnobenito@gmail.com>
2020-10-12 17:46:35 -0500

    Changed wording of prompts, removed placeholder text in length prompt for easier use on mobile

--- assets/java/pass.js
+++ assets/java/pass.js
@@ -10,7 +10,7 @@ let confirmLower;
 let Upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let Lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let Numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-let Special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?"];
+let Special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "{", "}", " ","+", "=", "-", "_"];
 
 
 //USER CLICKS BUTTON ON PAGE TO GENERATE PASSWORD:
@@ -19,7 +19,7 @@ function generate(){
 //USER RECEIVES PROMPTS FOR PASSWORD CRITERIA, STARTING WITH PASSWORD LENGTH:
 
 do {    //USING NUMBER(PROMPT TO CHANGE STRING ANSWER TO NUMBER VALUE:
-    length = Number(prompt("How many characters would you like your password to be? Please choose a number between 8 and 128", "type answer here"));
+    length = Number(prompt("How many characters would you like your password to be? Please choose a number between 8 and 128"));
 } 
 while (isNaN(length) || length < 8 || length > 128);
 //WHILE LOOP RUNS UNTIL VALID ANSWER IS RECEIVED
@@ -27,10 +27,10 @@ while (isNaN(length) || length < 8 || length > 128);
 //AFTER LENGTH IS VALIDATED, SERIES OF PROMPTS COME FOR PASSWORD CRITERIA:
 do {
     alert("please choose one or more of the following:");
-    confirmUpper =confirm("Would you like this password to include uppercase letters?");
-    confirmLower =confirm("Would you like this password to include lowercase letters?");
-    confirmNumbers =confirm("Would you like this password to include numbers?");
-    confirmSpecial =confirm("Would you like this password to include special characters?");
+    confirmUpper =confirm("Choose OK if you would like to include UPPERCASE characters in your password");
+    confirmLower =confirm("Choose OK if you would like to include LOWERCASE characters in your password");
+    confirmNumbers =confirm("Choose OK if you would like to include NUMBERS in your password");
+    confirmSpecial =confirm("Choose OK if you would like to include SPECIAL CHARACTERS in your password");
 }
 // AT LEAST ONE CRITERIA MUST BE MET, OTHERWISE ALERT AND REPROMPT
 while (!confirmUpper && !confirmLower && !confirmNumbers && !confirmSpecial); {
