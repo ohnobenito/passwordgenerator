9d26168fa64d401ec9e6b13725bc5983e72704aa
Benito Gonzalez <ohnobenito@gmail.com>
2020-10-17 18:07:43 -0500

    Added validation alerts for user selections

--- assets/java/pass.js
+++ assets/java/pass.js
@@ -9,9 +9,9 @@ let confirmLower;
 //THE FOLLOWING ARRAYS ARE USED FOR USER PROMPTS:
 let Upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let Lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let Numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+let Numbers = ["1","2", "3", "4", "5", "6", "7", "8", "9", "0"];
 let Special = [" ", "!", "\"", "#", "$", "%", "&","'","(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
-
+let password = "";
 
 //USER CLICKS BUTTON ON PAGE TO GENERATE PASSWORD:
 function generate(){
@@ -22,6 +22,8 @@ do {    //USING NUMBER(PROMPT TO CHANGE STRING ANSWER TO NUMBER VALUE:
     length = Number(prompt("How many characters would you like your password to be? Please choose a number between 8 and 128"));
 } 
 while (isNaN(length) || length < 8 || length > 128);
+alert("You've requested your password is " + length + " characters long");
+
 //WHILE LOOP RUNS UNTIL VALID ANSWER IS RECEIVED
  
 //AFTER LENGTH IS VALIDATED, SERIES OF PROMPTS COME FOR PASSWORD CRITERIA:
@@ -35,25 +37,35 @@ do {
 // AT LEAST ONE CRITERIA MUST BE MET, OTHERWISE ALERT AND REPROMPT
 while (!confirmUpper && !confirmLower && !confirmNumbers && !confirmSpecial); {
 } //WHILE LOOP RUNS UNTIL VALID CHOICE IS SELECTED
+
     
 // FILLING IN USER CRITERIA
 // USE OF CONCAT TO COMBINE ARRAYS AS USER CHOOSES
-let password=[""]
+let password=[]
     
 if (confirmUpper) {
-    password = Upper;
+    alert("You've requested your password contains UPPERCASE");
+    password = password.concat(Upper);
+    //ADD AT LEAST ONE IF TRUE
 }
 if (confirmLower) {
+    alert("You've requested your password contains LOWERCASE");
     password = password.concat(Lower);
+    //ADD AT LEAST ONE IF TRUE
 }
 if (confirmNumbers) {
-    password = password.concat(Numbers)
+    alert("You've requested your password contains NUMBERS");
+    password = password.concat(Numbers);
+    //ADD AT LEAST ONE IF TRUE
 }
 if (confirmSpecial) {
-    password =password.concat(Special)
+    alert("You've requested your password contains SPECIAL CHARACTERS");
+    password =password.concat(Special);
+    //ADD AT LEAST ONE IF TRUE
 }
 
 //AFTER PROMPTS, RUN A FOR LOOP WITH MATH RANDOM TO RANDOMIZE USER SELECTED CRITERIA AND GENERATE PASSWORD:
+
 let makePassword = ""
 
 for (let i = 0; i < length; i++) {
